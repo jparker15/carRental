4a30750b8324c5d1cb9417130f17356417301def
Jeremy <jeremyeparker@yahoo.com>
2021-11-15 20:21:25 -0500

    refactor name

--- src/main/java/com/expressian/carrental/repositories/VehicleRepository.java
+++ src/main/java/com/expressian/carrental/repositories/VehicleRepository.java
@@ -1,7 +1,7 @@
 package com.expressian.carrental.repositories;
 
-import com.expressian.carrental.models.Car;
+import com.expressian.carrental.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepository extends JpaRepository<Car,Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
 }
