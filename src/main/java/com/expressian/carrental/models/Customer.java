97aa2fab1e2703ed9b350d78c0b22ec1b1b409e3
Jeremy <jeremyeparker@yahoo.com>
2021-11-15 20:21:07 -0500

    new model

--- src/main/java/com/expressian/carrental/models/Customer.java
+++ src/main/java/com/expressian/carrental/models/Customer.java
@@ -1,2 +1,10 @@
-package com.expressian.carrental.models;public class Customer {
+package com.expressian.carrental.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Customer {
+    @Id @GeneratedValue private Long id;
+
+
 }
